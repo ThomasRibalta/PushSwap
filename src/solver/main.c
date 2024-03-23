562f4fa96b7562c54933984fc13e8eb53d41df78
Thomas <thomasribalta@gmail.com>
2024-03-23 14:51:53 +0100

    Add 1 Error on main

--- src/solver/main.c
+++ src/solver/main.c
@@ -6,7 +6,10 @@ int	main(int ac, char **av)
 
 	a = NULL;
 	if (ac < 2 || (ac == 2 && !av[1][0]))
+	{
+		write(1, "Error\n", 6);
 		return (0);
+	}
 	else if (ac == 2)
 		parsing(&a, ft_split(av[1], ' '), 1);
 	else
